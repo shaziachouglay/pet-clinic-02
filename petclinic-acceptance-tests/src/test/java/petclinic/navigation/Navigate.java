42b70e31713121f5fd3b1b286d25d0675933841c
shazia <shazia.chouglay@whitbread.com>
2021-06-22 20:08:33 +0100

    practicing pet clinic

--- petclinic-acceptance-tests/src/test/java/petclinic/navigation/Navigate.java
+++ petclinic-acceptance-tests/src/test/java/petclinic/navigation/Navigate.java
@@ -6,7 +6,7 @@ import net.serenitybdd.screenplay.actions.Open;
 
 public class Navigate {
     public static Performable toTheApplicationHomePage() {
-        return Task.where("{0) opens the application home page}",
+        return Task.where("{0} opens the application home page",
                 Open.browserOn().the(PetClinicHomePage.class));
 
     }
